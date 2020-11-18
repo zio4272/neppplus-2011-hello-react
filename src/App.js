337ddebc7b62f7ec0214d2149e2f71d86ee71b7d
zio <zio4272@naver.com>
2020-11-18 20:32:45 +0900

    헬로우~

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var logo = "2.gif"
+  var text = "겪하게 아무것도 하기 싫다..";
+  var number = (100+30) * 7 / 7;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {text}<br /><span className='num'>{number}</span>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          google
         </a>
       </header>
     </div>
